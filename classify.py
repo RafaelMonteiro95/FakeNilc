d26ad195216b7f71ac2c3c46ad404a85bb67edc6
Rafael Monteiro <rafaelmonteiro95@gmail.com>
2018-04-24 04:42:54 +0000

    classify.py now generates five cross validation results for five different percentiles of the dataset

--- classify.py
+++ classify.py
@@ -2,8 +2,7 @@ import argparse
 import pandas as pd 
 import sys
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import classification_report 
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report , confusion_matrix, accuracy_score
 from sklearn.utils import shuffle
 from sklearn.svm import SVC, LinearSVC
 
@@ -12,23 +11,35 @@ from sklearn.svm import SVC, LinearSVC
 def parseArguments():
 	arg_parser = argparse.ArgumentParser(description='A fake news classifier training system')
 	arg_parser.add_argument('dataset_filename', help='path to the file used as dataset')
+	arg_parser.add_argument('--n_jobs', help='number of threads to use when cross validating')
 	arg_parser.add_argument('-v','--verbose', help='output verbosity.', action='store_true')
 	args = arg_parser.parse_args()
 
 	### Parameters
 	dataset_filename = args.dataset_filename
 	verbose = args.verbose
+	if args.n_jobs != 0:
+		n_jobs = args.n_jobs
+	else:
+		n_jobs = 2
 
-	return (dataset_filename, verbose)
+	return (dataset_filename, verbose, n_jobs)
 
 
-def printResults(real, predicted, f=sys.stdout):
+def printResults(real, predicts, f=sys.stdout):
+	print('Learning curve values:')
+	#printing learning curve
+	v = len(real)
+	s = [int(v*0.2), int(v* 0.4), int(v*0.6), int(v*0.8), v]
+	accs = [100*accuracy_score(real[:s[i]], predicts[i]) for i in range(len(predicts))]
+	print('00.0000(0%) {0:.4f}(20%) {1:.4f}(40%) {2:.4f}(60%) {3:.4f}(80%) {4:.4f}(100%)'.format(*accs))
 
 	#printing classification report
-	print(classification_report(real,predicted), file = f)
+	print('Classification Report:')
+	print(classification_report(real,predicts[4]), file = f)
 
 	#printing confusion matrix
-	tn, fp, fn, tp = confusion_matrix(real, predicted).ravel()
+	tn, fp, fn, tp = confusion_matrix(real, predicts[4]).ravel()
 	print('Confusion Matrix:', file = f)
 	print(' a      b     <--- Classified as', file = f)
 	print('{0:5d}  {1:5d}   a = REAL'.format(tp,fp), file = f)
@@ -56,9 +67,27 @@ def getDatasetValues(df):
 	return (X, y)
 
 
+def predictAndEvaluate(classifier, X, y, n_jobs = 2, verbose = False):
+
+	#calculating slices of the dataset
+	v = len(y)
+	s = [int(v*0.2), int(v* 0.4), int(v*0.6), int(v*0.8), v]
+
+	predicts = [
+				cross_val_predict(classifier, X[:s[0]], y[:s[0]], cv=5, verbose=verbose, n_jobs=n_jobs),
+				cross_val_predict(classifier, X[:s[1]], y[:s[1]], cv=5, verbose=verbose, n_jobs=n_jobs),
+				cross_val_predict(classifier, X[:s[2]], y[:s[2]], cv=5, verbose=verbose, n_jobs=n_jobs),
+				cross_val_predict(classifier, X[:s[3]], y[:s[3]], cv=5, verbose=verbose, n_jobs=n_jobs),
+				cross_val_predict(classifier, X[:s[4]], y[:s[4]], cv=5, verbose=verbose, n_jobs=n_jobs),
+	]
+
+	return predicts
+
+
 def main():
 
-	dataset_filename, verbose = parseArguments()
+	#parsing arguments from command line
+	dataset_filename, verbose, n_jobs = parseArguments()
 	
 	if(verbose):
 		print(sys.argv[1:])
@@ -68,14 +97,19 @@ def main():
 	df = loadDataset(dataset_filename)
 
 	# getting data and labels from dataframe
+	# X = data, y = labels
 	X, y = getDatasetValues(df)
 
 	#preparing classifier
-	classifier = LinearSVC(C=1.0,verbose=verbose)
-	predicted = cross_val_predict(classifier, X, y, cv=5, verbose=verbose, n_jobs=2)
+	svm = LinearSVC(C=1.0,verbose=verbose)
 
-	#result:
-	printResults(y, predicted)
+	#trains the classifier using 5-fold cv and generates an array with results
+	#predicts contains the cross validation result for 5 percentiles of the dataset used:
+	#20%, 40%, 60%, 80% and 100%
+	predicts = predictAndEvaluate(svm, X, y, n_jobs, verbose)
+
+	#printing the result
+	printResults(y, predicts)
 
 if __name__ == '__main__':
 	main()
